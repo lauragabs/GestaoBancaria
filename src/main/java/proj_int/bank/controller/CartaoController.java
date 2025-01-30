b415edde9dfb91dac69d897a8146ebe3f7a96097
laura <laura.587523la@gmail.com>
2025-01-30 11:45:13 -0300

    adiciona controlador para gerenciamento de cartões

--- src/main/java/proj_int/bank/controller/CartaoController.java
+++ src/main/java/proj_int/bank/controller/CartaoController.java
@@ -1,5 +1,20 @@
 package proj_int.bank.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import proj_int.bank.repository.CartaoRepository;
+
+@Controller
+@RequestMapping("/cartao")
 public class CartaoController {
+
+    private final CartaoRepository cartaoRepository;
+
+    public CartaoController(CartaoRepository cartaoRepository) {
+        this.cartaoRepository = cartaoRepository;
+    }
+
+    
 
 }
